43d9f61d5e8b9f2211f2a608db0c29eb8e200a2b
mwndigi <55053517+mwndigi@users.noreply.github.com>
2022-09-06 23:05:42 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+server = http.createServer(function (req, res) {
+    res.writeHead(200, 
+        {'Content-Type': 'text/plain'});
+
+    res.write('Hello\r\n');
+
+    setTimeout(function () {
+        res.end('World\r\n');
+        }, 2000);
+    });
+
+server.listen(8000);
